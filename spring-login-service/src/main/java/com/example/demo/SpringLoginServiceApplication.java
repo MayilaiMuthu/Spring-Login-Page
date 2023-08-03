5caff45b0ed1d7ede64f2c1dd1a682e14ef032a2
MayilaiMuthu <muthube2020@gmail.com>
2023-08-03 07:42:56 +0530

    Initial commit.......

---
+++ spring-login-service/src/main/java/com/example/demo/SpringLoginServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MayilaiMuthu
+ * @since 03-08-2023
+ *
+ */
+
+@SpringBootApplication
+public class SpringLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginServiceApplication.class, args);
+	}
+
+}
